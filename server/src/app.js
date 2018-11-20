e6ce6ef942eb4a4b0002939992de2fc5514accb0
Hasin Ibtida <hasin.ibtida@gmail.com>
2018-11-20 22:20:13 +0600

    Server and Client Full Setup Completed!

--- server/src/app.js
+++ server/src/app.js
@@ -1,17 +1,23 @@
+//Initialize Required Modules
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 
+//Initialize Express Module
 const app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/status', (req, res) => {
+//Api Routes
+app.post('/register', (req, res) => {
   res.send({
-    message : 'Hello'
+    message: `Your user was registered!`,
+    email: req.body.email,
+    password:req.body.password
   });
 });
 
-app.listen(process.env.PORT || 8081)
+//Start Server
+app.listen(process.env.PORT || 8081);
